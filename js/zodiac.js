c0a97b03a3c31b2ec2b4acb78de48b43c5b90194
libertysyarn <code@libertysyarn.com>
2016-02-17 16:01:40 -0500

    lunar new year

--- js/zodiac.js
+++ js/zodiac.js
@@ -49,29 +49,23 @@
 //     return zodiacSigns.sagittarius;
 //   }
 // }
-//
-//
-// var chineseZodiac = function(animalResult) {
-//     var chineseAnimal = function() {
-//       var year = prompt("Enter an year plz:");
-//       var animal = [Monkey, Rooster, Dog, Pig, Rat, Ox, Tiger, Rabbit, Dragon, Snake, Horse, Sheep];
-//       var a === year % 12;
-//       return "Your chinese zodiac sign is the " + animal[a];
-//
-//     };
-//     chineseZodiac(animalResult);
 
-function lunarNew(year) {
-  var year;
-  for (i = 0; year < i; i++);
-  var startYear = [1912, 1931, 1950, 1969, 1988, 2007, 2026, 2045, 2064, 2083];
-  var newMoon = [
-    (startYear[i], 1, 3), ((startYear[i] + 1), 1, 6), ((startYear[i] + 2), 0, 26), ((startYear[i] + 3), 1, 14), ((startYear[i] + 4), 1, 3), ((startYear[i] + 5), 0, 23), ((startYear[i] + 6), 1, 11), ((startYear[i] + 7), 0, 31), ((startYear[i] + 8), 1, 19), ((startYear[i] + 9), 1, 8), ((startYear[i] + 10), 0, 28), ((startYear[i] + 11), 1, 15), ((startYear[i] + 12), 1, 5), ((startYear[i] + 13), 0, 24), ((startYear[i] + 14), 1, 12), ((startYear[i] + 15), 1, 2), ((startYear[i] + 16), 0, 22), ((startYear[i] + 17), 1, 9), ((startYear[i] + 18), 0, 29)
-  ];
 
-  var yearA = startYear[i];
-  var lunar = newMoon[yearA - year];
-  return lunar;
-  // var newYearDate = [feb 17, feb 6, jan 26, feb 14, feb 3, jan 23, feb 11, jan 31, feb 19, feb 8, jan 28, feb 15, feb 5, jan 24, feb 12, feb 2, jan 22, feb 9, jan 29]
+function lunarNew(year, month, day) {
+  var lunarMonth = [1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0];
+  var lunarDay = [17, 6, 26, 14, 3, 23, 11, 31, 19, 8, 28, 15, 5, 24, 12, 2, 22, 9, 29];
+  var cycle = (year - 1912) % 19;
+
+  if (month-1 < lunarMonth[cycle]) {
+    var year = year - 1;
+  } else if (day <= lunarDay[cycle]) {
+    var year = year - 1;
+  } else {
+  }
+
+  var z = year % 12;
+  var animal = ['Monkey', 'Rooster', 'Dog', 'Pig', 'Rat', 'Ox', 'Tiger', 'Rabbit', 'Dragon', 'Snake', 'Horse', 'Sheep'];
+  console.log(animal[z]);
+  return animal[z];
 }
-lunarNew(1975);
+lunarNew(1975, 2, 3);
